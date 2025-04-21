e9b3cd222b11d8cbbff6681e38b1961ee36b004d
Yasser Busati <YasserBusati@gmail.com>
2025-04-21 09:00:36 +0400

    Add ZigzagConversion implementation and tests; include problem statement and approach in documentation

---
+++ tests/Strings/ZigzagConversionTests.cs
@@ -0,0 +1,21 @@
+using CodingChallenges.Strings;
+
+namespace tests.Strings;
+
+public class ZigzagConversionTests
+{
+    [Theory]
+    [InlineData("PAYPALISHIRING", 3, "PAHNAPLSIIGYIR")]
+    [InlineData("PAYPALISHIRING", 4, "PINALSIGYAHRPI")]
+    [InlineData("A", 1, "A")]
+    [InlineData("AB", 1, "AB")]
+    [InlineData("ABC", 2, "ACB")]
+    public void Convert_ShouldReturnZigzagPattern(string input, int numRows, string expected)
+    {
+        // Act
+        var result = ZigzagConversion.Convert(input, numRows);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+}
